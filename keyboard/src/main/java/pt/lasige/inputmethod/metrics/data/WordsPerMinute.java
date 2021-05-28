a7bc4d3358d7f8ceb2385d9e3a7c23b958825a76
arbsantos <andre13521@gmail.com>
2021-05-28 15:05:06 +0100

    only calculate the wpm metric when the buffer has at least 5 chars

--- keyboard/src/main/java/pt/lasige/inputmethod/metrics/data/WordsPerMinute.java
+++ keyboard/src/main/java/pt/lasige/inputmethod/metrics/data/WordsPerMinute.java
@@ -1,7 +1,5 @@
 package pt.lasige.inputmethod.metrics.data;
 
-import android.util.Log;
-
 public class WordsPerMinute extends Metric {
     public WordsPerMinute() {
         super();
@@ -10,7 +8,9 @@ public class WordsPerMinute extends Metric {
     public double execute(String text, double time) {
 
         int textLen = text.length();
-
-        return (double) (textLen) / 5 * (60F / (time / 1000F)) ; //a "word" is any five characters
+        if(textLen < 5)
+            return -1; //we want to have at least 5 chars to calculate this metric
+        else
+            return (double) (textLen) / 5 * (60F / (time / 1000F)) ; //a "word" is any five characters
     }
 }
